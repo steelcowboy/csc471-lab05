7d78737d1ee53ccd73df4a9935843e4c521357f5
Ian Dunn <iondune@gmail.com>
2017-10-06 23:49:51 -0700

    Fix crash if shader file does not exist

--- src/Program.cpp
+++ src/Program.cpp
@@ -12,11 +12,18 @@ std::string readFileAsString(const std::string &fileName)
 	std::string result;
 	std::ifstream fileHandle(fileName);
 
-	fileHandle.seekg(0, std::ios::end);
-	result.reserve((size_t) fileHandle.tellg());
-	fileHandle.seekg(0, std::ios::beg);
+	if (fileHandle.is_open())
+	{
+		fileHandle.seekg(0, std::ios::end);
+		result.reserve((size_t) fileHandle.tellg());
+		fileHandle.seekg(0, std::ios::beg);
 
-	result.assign((std::istreambuf_iterator<char>(fileHandle)), std::istreambuf_iterator<char>());
+		result.assign((std::istreambuf_iterator<char>(fileHandle)), std::istreambuf_iterator<char>());
+	}
+	else
+	{
+		std::cerr << "Could not open file: '" << fileName << "'" << std::endl;
+	}
 
 	return result;
 }
